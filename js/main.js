077b79c5e27192a67d1c142fe3f5a1c0442f4825
Gerasin <2775748@gmail.com>
2016-02-02 20:36:31 +0300

    Корзина

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
 
 	var inp_but_text = $('.inp_but_text').val()
-	console.log(inp_but_text);
+	
 	$('.inp_but_minus').click(function() {
 		if ( inp_but_text > 0 ) {
 			inp_but_text = --inp_but_text;
@@ -57,6 +57,55 @@ $(document).ready(function() {
 		$('.maika_bascet_message').animate({ top : 72 }, 500);
 		return false;
 	});
+
+
+	// Количество товара
+	$('.quantity-plus').click(function(){
+		var inpuNamber = $(this).parent().find('input').val();
+		inpuNamber = ++inpuNamber;
+		$(this).parent().find('input').val(inpuNamber);
+		return false;
+	});
+	$('.quantity-minus').click(function(){
+		var inpuNamber = $(this).parent().find('input').val();
+		inpuNamber = --inpuNamber;
+		if(inpuNamber <= 0) {
+			inpuNamber = 1;
+		}
+		$(this).parent().find('input').val(inpuNamber);
+		return false;
+	});
+
+
+	// Табы доставки
+	$('.delivery-item:not(:first)').hide()
+
+	$('.express_delivery td').click(function(){
+		var trIndex = $(this).parents('tr').index();
+		trIndex = ++trIndex;
+		$(this).parents('.express_delivery').next('input').val(trIndex);
+		$('.express_delivery tr').removeClass('active');
+		$(this).parents('tr').addClass('active');
+	});
+	$('.delivery-tab a').click(function(){
+		$('.delivery-tab li').removeClass('active');
+		$(this).parent('li').addClass('active');
+		var tabOpen = $(this).attr('href');
+		$('.delivery-item').hide();
+		$(tabOpen).show();
+		return false;
+	});
+
+	$('.pay-select a').click(function(){
+		$('.pay-select a').removeClass('active');
+		$(this).addClass('active');
+		var payIndex = $(this).index();
+		payIndex = ++payIndex;
+		$(this).parent().find('input').val(payIndex);
+		return false;
+	})
+
+	
 
 
 });
